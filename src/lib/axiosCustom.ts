542acb9cc1c61f66d0c4ad61dba2ad0c623e0d78
tuanphampp9 <tuanphampp9@gmail.com>
2025-03-16 13:11:50 +0700

    fix timeout axios

--- src/lib/axiosCustom.ts
+++ src/lib/axiosCustom.ts
@@ -2,7 +2,6 @@ import axios from 'axios'
 
 const instant = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
-  timeout: 30000,
   headers: {
     'Content-Type': 'application/json',
   },
